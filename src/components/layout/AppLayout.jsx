3e2342b36c5e860bfee846d2448dd27ff06dce8b
Mohaddese Karimi <mhaddese@gmail.com>
2026-07-05 19:04:56 +0330

    style: add application header

--- src/components/layout/AppLayout.jsx
+++ src/components/layout/AppLayout.jsx
@@ -1,10 +1,14 @@
-import { Children } from "react"
-import Container from "../common/Container"
+import { Children } from 'react';
+import Container from '../common/Container';
+import Header from './Header';
 
 export default function AppLayout({Children}) {
-  return (
-    <main className="min-h-screen bg-slate-100">
-        <Container>{Children}</Container>
-    </main>
-  )
+    return (
+        <main className="min-h-screen bg-slate-100">
+            <Container>
+                <Header />
+                {Children}
+            </Container>
+        </main>
+    );
 }
