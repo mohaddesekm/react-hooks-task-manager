1e7c6a21dd798b29877ffdc5f66cb592db72ac37
Mohaddese Karimi <mhaddese@gmail.com>
2026-07-07 14:33:30 +0330

    bug fixes

--- src/components/task/TaskItem.jsx
+++ src/components/task/TaskItem.jsx
@@ -4,7 +4,7 @@ import { memo } from 'react';
 function TaskItem({ task, onRemove, onToggle }) {
     return (
         <li className="mt-3 flex justify-between items-center rounded-lg border bg-white p-4 dark:bg-zinc-800 dark:border-zinc-700">
-            <span className={`${task.completed && 'opacity-50 line-through'} text-gray-900 dark:text-white`}>
+            <span className={`${task.completed && 'opacity-50 line-through'} break-all text-gray-900 dark:text-white`}>
                 {task.title}
             </span>
             <div className="flex gap-2">
